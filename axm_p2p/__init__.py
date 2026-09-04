88d160e92ceaca7f96a4b7a7878b07db6a5d309b
mike-axiom-mir <miketobi90@gmail.com>
2026-09-04 22:45:48 +0200

    Export stable P2P layer API

--- axm_p2p/__init__.py
+++ axm_p2p/__init__.py
@@ -1,5 +1,19 @@
 """AXM P2P Direct Invite reference core."""
 from .invite import Invite, InviteError, create_invite, decode_invite
-from .udp import P2PHost, join_host
+from .layer import AXMP2PLayer, HostSession, JoinResult, LayerState
+from .udp import HandshakeError, P2PHost, Peer, join_host
 
-__all__ = ["Invite", "InviteError", "create_invite", "decode_invite", "P2PHost", "join_host"]
+__all__ = [
+    "AXMP2PLayer",
+    "HandshakeError",
+    "HostSession",
+    "Invite",
+    "InviteError",
+    "JoinResult",
+    "LayerState",
+    "P2PHost",
+    "Peer",
+    "create_invite",
+    "decode_invite",
+    "join_host",
+]
